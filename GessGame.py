879ef75774a26d4ef2e0431d4fb98329c8aa9df2
NickPCMR <osbornic@oregonstate.edu>
2020-05-31 21:44:19 -0700

    revc

--- GessGame.py
+++ GessGame.py
@@ -5,9 +5,16 @@
 import math
 
 class GessGame:
+    """class for playing a game of Gess, a chess/go variant.
+        X's represent white and O's represent black
+    """
     def __init__(self):
-        """" initializes game: sets status, whose turn it is, and starting
-        positions"""
+        """" initializes game:
+        status: unfinished
+        turn: "O"  (BLACK)
+        board: stones in starting positions
+        dictionary  for translating coordiantes
+    """
         self.__dict__ = {
             "X": "WHITE",
             "O": "BLACK",
@@ -76,7 +83,7 @@ class GessGame:
 
 
     def display(self):
-        """func for printing board to screen"""
+        """func for printing board to the screen"""
         for i in range(0, 20):
             for y in range(0, 20):
                 if y ==19:
@@ -85,6 +92,8 @@ class GessGame:
                     print(" " + self._board[i][y] + " " + "|", end="")
 
     def set_board(self, board):
+        """receives a board as an argument and assigns the game's board to
+        that value"""
         self._board = board
 
     def get_game_state(self):
@@ -92,14 +101,18 @@ class GessGame:
         return self._status
 
     def set_game_state(self, state):
-        """changes the current state of the game, options include UNFINISHED,
+        """receives a game state as an argument, changes the current state of
+        the game and prints it to the screen, options include UNFINISHED,
         BLACK_WON, WHITE_WON"""
         self._status = state
+        print(self.get_game_state())
 
     def get_turn(self):
+        """returns the state of the game"""
         return self._turn
 
     def set_turn(self):
+        """changes whose turn it is"""
         if self.get_turn() == "X":
             self._turn = "O"
 
@@ -114,8 +127,25 @@ class GessGame:
             self.set_game_state("WHITE_WON")
 
     def make_move(self, old_pos, new_pos):
-        """ takes 2 coordinates as input and will make the move if it is
-        legal"""
+        """ takes 2 strings  as coordinate pairs and will attempt to make a
+        legal move.
+
+        methods called:
+        get_game_state()
+        translate_coordinates()
+        get_piece()
+        validate_piece()
+        measure_distance()
+        legal_move()
+        get_next_pos()
+        get_coordinates
+        compare_coordinates()
+        get_stones()
+        move_piece()
+        clear_perimeter()
+        clear_footprint()
+        set_board
+        """
 
 
         # if the game is over return false
@@ -138,57 +168,90 @@ class GessGame:
         if not dist:
             return False
 
-        # check if piece has the necessary stones to move
+        # check if piece has the necessary stones to move in proposed direction
         valid = self.legal_move(piece,coordinates, dist)
         if not valid:
             return False
 
-        # returns a single x/y coordinate
+
+        # returns a single x/y coordinate for the next space in line  to
+        # the proposed move
         next_pos = self.get_next_pos(coordinates)
 
-        # get coordinates for next piece and the next one
+        # get coordinates [x,y] pairs for the current piece and the piece at
+        # the next coordinate position
         old_coord = self.get_coordinates(coordinates[0],coordinates[1])
         next_coord = self.get_coordinates(next_pos[0],next_pos[1])
 
-        # get a list of the new coordinates covered by move
+        # compare the 2 coordinate lists and return only the new coordinates
+        # that would be covered if moving one space
         unique_coords = self.compare_coordinates(old_coord, next_coord)
 
-        # check if those spaces contain characters
+        # check if the new spaces contain characters
         new_stones = self.get_stones(unique_coords)
 
+        # this loop will iterate based off of the distance the move,
+        # each time checking the new spaces covered
         for move in range(0, dist[0]):
+            # check to see if the new spaces contain stones
             if self.contains_stones(new_stones):
+
+                # if it contains stones,  check to see if it reached
+                # destination
+                #   if it  contains stones and the destination is  reached,
+                #   complete the move
                 if coordinates[2] == next_pos[0] and coordinates[3] == next_pos[1]:
-                    print("complete move")
+
+                    # create a copy of the board
                     board_copy = [stone[:] for stone in self._board]
                     self.move_piece(piece,next_coord)
                     self.clear_footprint(coordinates)
                     self.clear_perimeter()
 
+                    #  if the move broke the players ring (illegal move)
+                    #  return false and reset the board to the copy
                     if not self.check_win():
                         self.set_board(board_copy)
                         return False
+
+
+                    # if it reaches here, the move was legal and completed so
+                    # update the turn and return True
                     self.set_turn()
                     return True
 
+                # if the next space contains stones and the piece hasn't
+                # reached it's destination, return false
                 else:
                     print("overlapping footprint")
                     return False
+
+                # in the case the next space doesn't contain stones, check to
+                # see if it arrived at the destination
             else:
+
+                # complete the move
                 if coordinates[2] == next_pos[0] and coordinates[3] == next_pos[1]:
-                    print("complete move")
                     board_copy = [stone[:] for stone in self._board]
                     self.move_piece(piece,next_coord)
                     self.clear_footprint(coordinates)
                     self.clear_perimeter()
 
+                    # if the move broke the players ring, return False and
+                    # reset the board
                     if not self.check_win():
                         self.set_board(board_copy)
                         return False
                     self.set_turn()
                     return True
 
+                # if the next spaces are empty  and the  destination hasn't
+                # been reached, iterate the loop
                 else:
+
+                    # get_next_pos requires 4 coordinate arguments,
+                    # so  extend the
+                    # list to include the destination coordinates
                     next_pos.extend([coordinates[2],coordinates[3]])
                     next_pos = self.get_next_pos(next_pos)
                     old_coord = next_coord
@@ -197,21 +260,27 @@ class GessGame:
                     new_stones = self.get_stones(unique_coords)
 
     def check_win(self, x=2, y=2,o_ring=False,x_ring=False):
-        """check the board to see if both players have a ring"""
+        """recursively checks the board for a black ring (x_ring) and a white ring
+        (o_ring). starts in the top left corner  and checks every piece to
+        see if it contains a ring. calls get_piece() and get_turn().  Will
+        also check to see if a ring is broken out of turn (not a  winning
+        move) and will return False
+        """
 
-
-
-
+        # grab hte first piece
         piece = self.get_piece(x,y)
 
         x_count = 0
         o_count = 0
+
+        # count X's and O's
         for stone in piece:
             if stone == "X":
                 x_count += 1
             if stone == "O":
                 o_count += 1
 
+        # if a ring is found change x_ring or o_ring to True
         if x_count == 8 and piece[4] == " ":
             x_ring = True
 
@@ -219,34 +288,32 @@ class GessGame:
             o_ring = True
 
 
-        # base case, reach end of board or 2 rings are found
-
+        # base case, reach end of board or a ring for each player is found
 
         if (x_ring is True and o_ring is True) or (x == 17 and y == 17):
+
+            # if a player broke their own ring, return False
             if x_ring is False and self.get_turn() == "X":
                 print("can't break X ring")
                 return False
-
 
             if o_ring is False and self.get_turn() == "O":
                 print("can't break O ring")
                 return False
 
-
+            #  if a winning move is detected, update gamestate
             if x_ring is False and self.get_turn() == "O":
                 self.set_game_state("BLACK_WON")
-                print(self.get_game_state())
                 return True
-
 
             if o_ring is False and self.get_turn() == "X":
                 self.set_game_state("WHITE_WON")
-                print(self.get_game_state())
                 return True
 
             if x_ring is True and o_ring is True:
                 return True
 
+        # when  the end  of a row is reached, restart on the next row
         if x == 17:
             x = 2
             y += 1
@@ -265,7 +332,7 @@ class GessGame:
 
 
     def clear_footprint(self, coordinates):
-        """sets old footprint to blank spaces unless they overlap with new
+        """sets old footprint to blank spaces unless they overlap with the new
         position"""
         old = self.get_coordinates(coordinates[0],coordinates[1])
         new = self.get_coordinates(coordinates[2],coordinates[3])
@@ -284,7 +351,7 @@ class GessGame:
 
     @staticmethod
     def contains_stones(char_list):
-        """receives a list of charcters from the board and determines if they
+        """receives a list of characters from the board and determines if they
         are blank or stones, returns T if blank, F otherwise"""
         char_count = 0
         for char in char_list:
@@ -310,8 +377,8 @@ class GessGame:
 
     @staticmethod
     def compare_coordinates(coord1, coord2):
-        """ compare 2 pieces' coordinates and remove any coordinates that
-        overlap"""
+        """ compare 2 pieces' coordinates and return the  coordinates unique
+        to coord2"""
         new_coords = []
         for x in coord2:
             if x not in coord1:
@@ -386,8 +453,9 @@ class GessGame:
 
     @staticmethod
     def measure_distance(coordinates):
-        """receives a list of coordinates and returns the distance between 2
-        points. Returns False if coordiantes are the same or if move is not
+        """receives a list of coordinates and returns the distance and slope
+        between the 2 points. Returns False if coordinates are the same or if
+        move is not
         diagonal or straight"""
         x1 = coordinates[0]
         x2 = coordinates[2]
@@ -405,6 +473,7 @@ class GessGame:
         if slope == 1.0:
             distance = abs(x1-x2)
 
+        # impossible angle detected
         if slope not in (1.0,0):
             print("impossible slope")
             return False
@@ -419,7 +488,6 @@ class GessGame:
         x2 = coordinates[2]
         y1 = coordinates[1]
         y2 = coordinates[3]
-        slope = distance[1]
         dist = distance[0]
 
         # cannot travel  more than 3 spaces without center piece
@@ -471,7 +539,7 @@ class GessGame:
         return True
 
     def translate_coordinates(self, old, new):
-        """takes 2 input strings as coordinates and converts them to list
+        """receives 2 strings as coordinates and converts them to list
         indices for the board, returns a list of integers as x and y
         coordinates;
         returns False if invalid input"""
@@ -481,6 +549,10 @@ class GessGame:
         y1 = ""
         y2 = ""
 
+        # use the dictionary to look up a letter's corresponding board index
+        # integer value for columns
+        # use string minus it's first character and convert it to an integer
+        # to get the row value
         try:
             x1 = self.__dict__[old[0].lower()]
             y1 = 20 - int(old[1:])
@@ -493,7 +565,7 @@ class GessGame:
             print("invalid coordinates")
             return False
 
-        # if coordinates are not withing the 18x18 grid return False
+        # if coordinates are not within the 18x18 grid return False
         if y1 not in range(1,19) or y2 not in range(1,19) or x1 not in range(
                 1,19) or x2 not in range(1,19):
             print("out of range")
@@ -501,8 +573,9 @@ class GessGame:
         return [x1,y1,x2,y2]
 
     def get_piece(self, x, y):
-        """accepts a list with an x and y coordinate, returns a list  of the
-        surrounding piece values"""
+        """accepts an x and y coordinate, returns a list  of the
+        surrounding piece's stones"""
+
         piece = []
 
         piece.append(self._board[y-1][x-1])     # NW
@@ -539,9 +612,11 @@ class GessGame:
         if x_count > 0 and o_count > 0:
             print("piece contains multiple players' pieces")
             return False
+
         if x_count == 0 and o_count == 0:
             print("blank piece")
             return False
+
         if piece[4] != ' ' and blank_count == 8:
             print("single center piece")
             return False
